99df851fdc10e1ddbfecb103ae8ce7fd8d543152
YDP <nightmareydp@163.com>
2022-08-19 01:46:10 +0800

    Config配置中心相关代码

---
+++ cloud-config-client-3355/src/main/java/com/ydp/spring/ConfigClientMain3355.java
@@ -0,0 +1,18 @@
+package com.ydp.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author YDP
+ * @version 1.0
+ * @date 2022/8/19 1:34
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class ConfigClientMain3355 {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientMain3355.class, args);
+    }
+}
---
+++ cloud-config-client-3355/src/main/java/com/ydp/spring/controller/ConfigClientController.java
@@ -0,0 +1,26 @@
+package com.ydp.spring.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author YDP
+ * @version 1.0
+ * @date 2022/8/19 1:35
+ */
+@RestController
+@RefreshScope
+public class ConfigClientController
+{
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/configInfo")
+    public String getConfigInfo()
+    {
+        return configInfo;
+    }
+}
+
